48e3602dc931b68e3ff07ed017c897430239dd06
Naman Gogia <namangogia2001@gmail.com>
2022-10-04 01:22:45 +0530

    feat: add user login

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,86 +1,135 @@
-
-import React, { useState } from "react"
+import React, { useState } from "react";
 
 export default function Auth(props) {
-    let [authMode, setAuthMode] = useState("signin")
+  let [authMode, setAuthMode] = useState("signin");
+  let [username, setUsername] = useState("");
+  let [password, setPassword] = useState("");
 
-    const changeAuthMode = () => {
-        setAuthMode(authMode === "signin" ? "signup" : "signin")
-    }
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    const url = "https://api-certi-portal.herokuapp.com/auth/login";
+    const options = {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    };
+    const response = await fetch(url, options);
 
-    if (authMode === "signin") {
-        return (
-            <div className="Auth-form-container">
-                <form className="Auth-form">
-                    <div className="Auth-form-content">
-                        <h3 className="Auth-form-title">Sign In</h3>
-                        <div className="text-center">
-                            Not registered yet?{" "}
-                            <span className="link-primary" onClick={changeAuthMode}>
-                                Sign Up
-                            </span>
-                        </div>
-                        <div className="form-group mt-3">
-                            <label>Username</label>
-                            <input
-                                type="text"
-                                className="form-control mt-1"
-                                placeholder="Enter Username"
-                            />
-                        </div>
-                        <div className="form-group mt-3">
-                            <label>Password</label>
-                            <input
-                                type="password"
-                                className="form-control mt-1"
-                                placeholder="Enter password"
-                            />
-                        </div>
-                        <div className="d-grid gap-2 mt-3">
-                            <button type="submit" className="btn btn-primary">
-                                Submit
-                            </button>
-                        </div>
-                    </div>
-                </form>
-            </div>
-        )
-    }
+    if (response.status === 200) console.log("Logged in");
+    else console.log("Login failed");
+  };
 
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    const url = "https://api-certi-portal.herokuapp.com/api/user";
+    const options = {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    };
+    const response = await fetch(url, options);
+
+    if (response.status === 200) {
+      console.log("Registered");
+      console.log(response);
+    } else console.log("Login failed");
+  };
+
+  const changeAuthMode = () => {
+    setAuthMode(authMode === "signin" ? "signup" : "signin");
+  };
+
+  if (authMode === "signin") {
     return (
-        <div className="Auth-form-container">
-            <form className="Auth-form">
-                <div className="Auth-form-content">
-                    <h3 className="Auth-form-title">Sign In</h3>
-                    <div className="text-center">
-                        Already registered?{" "}
-                        <span className="link-primary" onClick={changeAuthMode}>
-                            Sign In
-                        </span>
-                    </div>
-                    <div className="form-group mt-3">
-                        <label>Username</label>
-                        <input
-                            type="text"
-                            className="form-control mt-1"
-                            placeholder="Enter Username"
-                        />
-                    </div>
-                    <div className="form-group mt-3">
-                        <label>Password</label>
-                        <input
-                            type="password"
-                            className="form-control mt-1"
-                            placeholder="Password"
-                        />
-                    </div>
-                    <div className="d-grid gap-2 mt-3">
-                        <button type="submit" className="btn btn-primary">
-                            Submit
-                        </button>
-                    </div>
-                </div>
-            </form>
+      <div className="Auth-form-container">
+        <form className="Auth-form">
+          <div className="Auth-form-content">
+            <h3 className="Auth-form-title">Sign In</h3>
+            <div className="text-center">
+              Not registered yet?{" "}
+              <span className="link-primary" onClick={changeAuthMode}>
+                Sign Up
+              </span>
+            </div>
+            <div className="form-group mt-3">
+              <label>Username</label>
+              <input
+                type="text"
+                className="form-control mt-1"
+                placeholder="Enter Username"
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+            <div className="form-group mt-3">
+              <label>Password</label>
+              <input
+                type="password"
+                className="form-control mt-1"
+                placeholder="Enter password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div className="d-grid gap-2 mt-3">
+              <button
+                type="submit"
+                className="btn btn-primary"
+                onClick={handleLogin}
+              >
+                Login
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    );
+  }
+
+  return (
+    <div className="Auth-form-container">
+      <form className="Auth-form">
+        <div className="Auth-form-content">
+          <h3 className="Auth-form-title">Sign Up</h3>
+          <div className="text-center">
+            Already registered?{" "}
+            <span className="link-primary" onClick={changeAuthMode}>
+              Sign In
+            </span>
+          </div>
+          <div className="form-group mt-3">
+            <label>Username</label>
+            <input
+              type="text"
+              className="form-control mt-1"
+              placeholder="Enter Username"
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div className="form-group mt-3">
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control mt-1"
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="d-grid gap-2 mt-3">
+            <button
+              type="submit"
+              className="btn btn-primary"
+              onClick={handleRegister}
+            >
+              Sign Up
+            </button>
+          </div>
         </div>
-    )
+      </form>
+    </div>
+  );
 }

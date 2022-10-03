835e07989182b37c75f24bfa38c910197f04e04a
Abhinandan Sharma <abhi.moudgil15@gmail.com>
2022-10-04 01:39:11 +0530

    added preview

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,53 +1,93 @@
 import React, { useState, useEffect } from "react";
 import { addDataToCerti } from "../utils/converter";
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import { useParams } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 function DashForm() {
-  const [logo, setLogo] = useState("");
-  const [sign, setSign] = useState("");
-  const [instname, setInstname] = useState("");
-  const [username, setUsename] = useState("");
-  const [certiTemplate, setCertiTemplate] = useState({})
+  const [logo, setLogo] = useState("https://i.ibb.co/cDNy1Jq/6c4baa6e-3e9f-4919-b1a7-b8622892cf84file-741123.webp");
+  const [sign, setSign] = useState("https://i.ibb.co/23yn7dW/Whats-App-Image-2022-09-15-at-12-35-01-AM.jpg");
+  const [Name, setName] = useState("");
+  const [isEnable,setEnable]=useState(true);
+//   const [username, setUsename] = useState("");
+  const [certiTemplate, setCertiTemplate] = useState({});
+  let { id } = useParams();
   const onSubmit = (e) => {
     e.preventDefault();
+    setEnable(false)
     const data = {
-      logo,
-      sign,
-      instname,
-      username,
+      Name,
     };
-    addDataToCerti(certiTemplate, data);
+    const pictData = {
+        institutionLogo:logo,
+        institutionSignature:sign,
+    };
+    addDataToCerti(certiTemplate, data, pictData);
   };
   useEffect(() => {
-    fetch("https://api-certi-portal.herokuapp.com/api/certificate/1").then((data) => {
-      data.json().then((res) => {
-        console.log(res)
-        setCertiTemplate(res.data.content)
-      })
-    })
-  }, [])
+    fetch("https://api-certi-portal.herokuapp.com/api/template/" + id).then(
+      (data) => {
+        data.json().then((res) => {
+          console.log(res);
+          setCertiTemplate(res.data.html);
+        });
+      }
+    );
+  }, []);
   return (
     <div className="form-container">
       <h1>Add Your Information</h1>
       <form className="dash-form" onSubmit={onSubmit}>
         <div className="form-group">
           <div>
-            <input type="text" name="username" id="username" placeholder="Enter Username" onChange={(e) => { setUsename(e.target.value) }} />
+            <input
+              type="text"
+              name="username"
+              id="username"
+              placeholder="Enter Username"
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+            />
           </div>
-          <input type="text" name="logo" id="logo" placeholder="Enter Link for Logo of Institution" onChange={(e) => { setLogo(e.target.value) }} />
+          <input
+            type="text"
+            name="logo"
+            id="logo"
+            placeholder="Enter Link for Logo of Institution"
+            onChange={(e) => {
+              setLogo(e.target.value);
+            }}
+          />
         </div>
         <div>
-          <input type="text" name="sign" id="signature" placeholder="Enter link for Signature" onChange={(e) => { setSign(e.target.value) }} />
+          <input
+            type="text"
+            name="sign"
+            id="signature"
+            placeholder="Enter link for Signature"
+            onChange={(e) => {
+              setSign(e.target.value);
+            }}
+          />
         </div>
         <div>
-          <input type="text" name="instname" id="institute" placeholder="Enter Name of Institution" onChange={(e) => { setInstname(e.target.value) }} />
+          {/* <input
+            type="text"
+            name="instname"
+            id="institute"
+            placeholder="Enter Name of Institution"
+            onChange={(e) => {
+              setInstname(e.target.value);
+            }}
+          /> */}
         </div>
 
-        <button type="submit" className="btn btn-primary">Submit</button>
+        <button type="submit" className="btn btn-primary" disabled={!isEnable}>
+          Submit
+        </button>
       </form>
     </div>
-
   );
 }
 
--- src/utils/converter.js
+++ src/utils/converter.js
@@ -1,3 +1,26 @@
-export const addDataToCerti=(certificateTemplate,data)=>{
-    console.log(certificateTemplate,data)
-}
+export const addDataToCerti = (certificateTemplate, data, pictData) => {
+  let a = document.createElement("div");
+  console.log(typeof certificateTemplate);
+  a.innerHTML = certificateTemplate;
+  //   a.style.display = "none";
+  document.body.appendChild(a);
+  for (let i in data) {
+    if (document.querySelector("." + i))
+      document.querySelector("." + i).innerHTML = data[i];
+  }
+
+  for (let i in pictData) {
+    if (document.querySelector("." + i))
+      document.querySelector("." + i).src = pictData[i];
+  }
+
+  // eslint-disable-next-line no-undef
+  domtoimage
+    .toJpeg(document.getElementById("temp"), { quality: 0.95 })
+    .then(function (dataUrl) {
+      var link = document.createElement("a");
+      link.download = "my-image-name.jpeg";
+      link.href = dataUrl;
+      link.click();
+    });
+};

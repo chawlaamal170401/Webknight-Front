b042106d0b235b35868c135e37b3cc9c3d7ed40e
Amal Chawla <chawla.amal.170401@gmail.com>
2022-10-04 01:03:01 +0530

    Added styles to form

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -7,55 +7,55 @@ import { TbCertificate } from "react-icons/tb";
 import { Link } from "react-router-dom";
 
 function Dashboard() {
-  return (
-    <div className="dash-container">
-      <div className="dash-header">
-        <h1>Hello</h1>
-        <input type="search" placeholder="Search Certificate using id" />
-        <div className="dash-icon">
-          <a href="#">
-            <FaRegUserCircle size={50} />
-          </a>
-          <a href="#">
-            <MdLogout size={50} />
-          </a>
-        </div>
-      </div>
-      <div className="templates">
-        <Carousel variant="dark">
-          <Carousel.Item interval={1000}>
-            <Link to="/form/1234">
-              <img className="d-block w-50" src={pic} alt="First slide"     />
-            </Link>
-          </Carousel.Item>
-          <Carousel.Item interval={500}>
-            <img className="d-block w-50" src={pic} alt="Second slide" />
-          </Carousel.Item>
-          <Carousel.Item>
-            <img className="d-block w-50" src={pic} alt="Third slide" />
-          </Carousel.Item>
-        </Carousel>
-      </div>
+    return (
+        <div className="dash-container">
+            <div className="dash-header">
+                <h1>Hello</h1>
+                <input type="search" placeholder="Search Certificate using id" />
+                <div className="dash-icon">
+                    <a href="#">
+                        <FaRegUserCircle size={50} />
+                    </a>
+                    <a href="#">
+                        <MdLogout size={50} />
+                    </a>
+                </div>
+            </div>
+            <div className="templates">
+                <Carousel variant="dark">
+                    <Carousel.Item interval={1000}>
+                        <Link to="/form/1234">
+                            <img className="d-block w-50" src={pic} alt="First slide" />
+                        </Link>
+                    </Carousel.Item>
+                    <Carousel.Item interval={500}>
+                        <img className="d-block w-50" src={pic} alt="Second slide" />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img className="d-block w-50" src={pic} alt="Third slide" />
+                    </Carousel.Item>
+                </Carousel>
+            </div>
 
-      <div className="dash-grid">
-        <h4>History</h4>
-        <div className="dash-certi">
-          <a href="#">
-            <TbCertificate size={40} />
-            Cid
-          </a>
-          <a href="#">
-            <TbCertificate size={40} />
-            Cid
-          </a>
-          <a href="#">
-            <TbCertificate size={40} />
-            Cid
-          </a>
+            <div className="dash-grid">
+                <h4>History</h4>
+                <div className="dash-certi">
+                    <a href="#">
+                        <TbCertificate size={40} />
+                        Cid
+                    </a>
+                    <a href="#">
+                        <TbCertificate size={40} />
+                        Cid
+                    </a>
+                    <a href="#">
+                        <TbCertificate size={40} />
+                        Cid
+                    </a>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Dashboard;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,14 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { addDataToCerti } from "../utils/converter";
-function Form() {
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+function DashForm() {
   const [logo, setLogo] = useState("");
   const [sign, setSign] = useState("");
   const [instname, setInstname] = useState("");
   const [username, setUsename] = useState("");
-  const [certiTemplate,setCertiTemplate] = useState({})
+  const [certiTemplate, setCertiTemplate] = useState({})
   const onSubmit = (e) => {
     e.preventDefault();
     const data = {
@@ -14,25 +17,38 @@ function Form() {
       instname,
       username,
     };
-    addDataToCerti(certiTemplate,data);
+    addDataToCerti(certiTemplate, data);
   };
-  useEffect(()=>{
-    fetch("https://api-certi-portal.herokuapp.com/api/certificate/1").then((data)=>{
-        data.json().then((res)=>{
-            console.log(res)
-            setCertiTemplate(res.data.content)
-        })
+  useEffect(() => {
+    fetch("https://api-certi-portal.herokuapp.com/api/certificate/1").then((data) => {
+      data.json().then((res) => {
+        console.log(res)
+        setCertiTemplate(res.data.content)
+      })
     })
-  },[])
+  }, [])
   return (
-    <form className="" onSubmit={onSubmit}>
-      <input type="text" name="logo" id="" onChange={(e)=>{setLogo(e.target.value)}}/>
-      <input type="text" name="sign" id="" onChange={(e)=>{setSign(e.target.value)}}/>
-      <input type="text" name="instname" id="" onChange={(e)=>{setInstname(e.target.value)}}/>
-      <input type="text" name="username" id="" onChange={(e)=>{setUsename(e.target.value)}}/>
-      <button type="submit">Submit</button>
-    </form>
+    <div className="form-container">
+      <h1>Add Your Information</h1>
+      <form className="dash-form" onSubmit={onSubmit}>
+        <div className="form-group">
+          <div>
+            <input type="text" name="username" id="username" placeholder="Enter Username" onChange={(e) => { setUsename(e.target.value) }} />
+          </div>
+          <input type="text" name="logo" id="logo" placeholder="Enter Link for Logo of Institution" onChange={(e) => { setLogo(e.target.value) }} />
+        </div>
+        <div>
+          <input type="text" name="sign" id="signature" placeholder="Enter link for Signature" onChange={(e) => { setSign(e.target.value) }} />
+        </div>
+        <div>
+          <input type="text" name="instname" id="institute" placeholder="Enter Name of Institution" onChange={(e) => { setInstname(e.target.value) }} />
+        </div>
+
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+
   );
 }
 
-export default Form;
+export default DashForm;
